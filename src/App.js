6c61950f412a86a9c67d6821ae49055f7113805b
damcyk96 <damian.obajtek@gmail.com>
2022-06-26 20:46:16 +0200

    dupa

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { ThemeProvider } from "styled-components";
 import { useDarkMode } from "./components/useDarkMode";
 import { GlobalStyles } from "./components/Globalstyle";
 import { lightTheme, darkTheme } from "./components/Themes";
 import Toggle from "./components/Toggler";
 import "./App.css";
-import dummyData from "./data";
-import CardList from "./components/CardList";
 import { BrowserRouter } from "react-router-dom";
 import Main from "./pages/Main";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [theme, themeToggler, mountedComponent] = useDarkMode();
 
   const themeMode = theme === "light" ? lightTheme : darkTheme;
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVideos(dummyData);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
 
   if (!mountedComponent) return <div />;
 
